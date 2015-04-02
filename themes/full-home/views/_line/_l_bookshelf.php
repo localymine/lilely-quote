cfdafc62c465cbd441efd88504acaaff8a107ab4
Kantoh <leduongkhang@gmail.com>
2015-04-02 07:07:53 +0700

    Add link to book

--- themes/full-home/views/_line/_l_bookshelf.php
+++ themes/full-home/views/_line/_l_bookshelf.php
@@ -1,5 +1,5 @@
-<?php 
-foreach ($this->languages as $key => $value){
+<?php
+foreach ($this->languages as $key => $value) {
     if ($this->lang != $key) {
         $lang = $key;
         break;
@@ -16,15 +16,16 @@ foreach ($this->languages as $key => $value){
         <div class="bk-book book-1 bk-bookdefault">
             <div class="bk-front">
                 <div class="bk-cover bk-bookview" data-trans-id="<?php echo $row->id ?>">
-                    <a class="pl-icon-cover" href="<?php echo Yii::app()->createUrl($row->post_type . '/show', array('slug' => $row->slug)) ?>"><i class="fa fa-play fa-2x"></i></a>
-                    <?php if($row->cover != ''): ?>
-                        <img alt="<?php echo $row->post_title ?>" src="<?php echo Yii::app()->baseUrl ?>/images/<?php echo $row->post_type ?>/<?php echo $image_cover ?>" class="img-responsive">
-                    <?php else: ?>
-                        <h2>
-                            <span><?php echo $row->quote_author ?></span>
-                            <span><?php echo $row->post_title ?></span>
-                        </h2>
-                    <?php endif; ?>
+                    <a href="<?php echo Yii::app()->createUrl($row->post_type . '/show', array('slug' => $row->slug)) ?>">
+                        <?php if ($row->cover != ''): ?>
+                            <img alt="<?php echo $row->post_title ?>" src="<?php echo Yii::app()->baseUrl ?>/images/<?php echo $row->post_type ?>/<?php echo $image_cover ?>" class="img-responsive">
+                        <?php else: ?>
+                            <h2>
+                                <span><?php echo $row->quote_author ?></span>
+                                <span><?php echo $row->post_title ?></span>
+                            </h2>
+                        <?php endif; ?>
+                    </a>
                 </div>
                 <div class="bk-cover-back"></div>
             </div>
@@ -53,22 +54,23 @@ foreach ($this->languages as $key => $value){
             <a href="<?php echo Yii::app()->createUrl($row->post_type . '/show', array('slug' => $row->slug)) ?>"><button><i class="fa fa-play"></i></button></a>
         </div>
     </li>
-    
+
     <?php $model_line = Post::model()->localized($lang)->findByPk((int) $row->id); ?>
     <?php $image_cover = ($model_line->cover != '') ? $model_line->cover : '0.jpg'; ?>
     <li class="trans-book-back hide" id="trans-id-<?php echo $row->id ?>">
         <div class="bk-book book-1 bk-bookdefault">
             <div class="bk-front">
                 <div class="bk-cover bk-bookview" data-trans-id="<?php echo $row->id ?>">
-                    <a class="pl-icon-cover" href="<?php echo Yii::app()->createUrl($row->post_type . '/show', array('slug' => $row->slug)) ?>"><i class="fa fa-play fa-2x"></i></a>
-                    <?php if($row->cover != ''): ?>
-                        <img alt="<?php echo $model_line->post_title ?>" src="<?php echo Yii::app()->baseUrl ?>/images/<?php echo $row->post_type ?>/<?php echo $image_cover ?>" class="img-responsive">
-                    <?php else: ?>
-                        <h2>
-                            <span><?php echo $row->quote_author ?></span>
-                            <span><?php echo $model_line->post_title ?></span>
-                        </h2>
-                    <?php endif; ?>
+                    <a href="<?php echo Yii::app()->createUrl($row->post_type . '/show', array('slug' => $row->slug)) ?>">
+                        <?php if ($row->cover != ''): ?>
+                            <img alt="<?php echo $model_line->post_title ?>" src="<?php echo Yii::app()->baseUrl ?>/images/<?php echo $row->post_type ?>/<?php echo $image_cover ?>" class="img-responsive">
+                        <?php else: ?>
+                            <h2>
+                                <span><?php echo $row->quote_author ?></span>
+                                <span><?php echo $model_line->post_title ?></span>
+                            </h2>
+                        <?php endif; ?>
+                    </a>
                 </div>
                 <div class="bk-cover-back"></div>
             </div>
@@ -97,7 +99,7 @@ foreach ($this->languages as $key => $value){
             <a href="<?php echo Yii::app()->createUrl($row->post_type . '/show', array('slug' => $model_line->slug, 'language' => $lang)) ?>"><button><i class="fa fa-play"></i></button></a>
         </div>
     </li>
-   
+
 </ul>
 
 <?php
