8dfdc1b59f630242703557c5086170c4c39a2a75
Kantoh <leduongkhang@gmail.com>
2015-06-17 08:42:03 +0700

    Advertise

--- themes/full-home/views/advertise/index.php
+++ themes/full-home/views/advertise/index.php
@@ -5,12 +5,12 @@ $this->pageTitle = 'Advertise' . '|' . Yii::app()->name;
 <div class="min-height show-content advertise">
     <div class="row center-block intro">
         <div class="col-md-4 col-xs-12 first">
-            <img class="img-responsive center-block" src="../images/lilely-tree.png"/>
+            <img class="img-responsive center-block" src="../images/logo.png"/>
         </div>
         <div class="col-md-4 col-xs-12 mid">
-            <h1>Test for the Advertise with Us:</h1>
+            <h1>Why should you advertise with Lilely?</h1>
             <p>
-                "Lilely is a mindful way to inspire yourself and others. Advertising with us involves sponsorship, where you can have an opportunity to thousands of thoughtful and inspired people and let them promote your product to their friends and other members of the network. Sponsorship means that you will be the sponsor of the day, and have the entire page to yourself, rather than competing for the attention of people logged on to the site. Contact us today through the form below to discuss rates and how we can help your brand create an inspired reputation."
+                Lilely is a mindful way to inspire yourself and others. Advertising with us involves sponsorship, where you can have an opportunity to thousands of thoughtful and inspired people and let them promote your product to their friends and other members of the network. Sponsorship means that you will be the sponsor of the day, and have the entire page to yourself, rather than competing for the attention of people logged on to the site. Contact us today through the form below to discuss rates and how we can help your brand create an inspired reputation.
             </p>
         </div>
         <div class="col-md-4 col-xs-12 last">
